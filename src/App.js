52652233529514ed0c5971f35f75916682bbeb1b
yara-shimizu <4bjj3228.ryota@gmail.com>
2019-08-03 15:38:09 +0900

    不要なファイルを削除

---
+++ src/App.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <Fragment>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic">
+          <View>
+            <View>
+            </View>
+            <View>
+            </View>
+            <View>
+            </View>
+            <View>
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </Fragment>
+  );
+};
+
+export default App;
