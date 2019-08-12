7d72cb0241785eedc5d47734ff84286ff55e284c
yara-shimizu <4bjj3228.ryota@gmail.com>
2019-08-13 04:29:40 +0900

    reduxにコメントを追加

--- src/redux.js
+++ src/redux.js
@@ -4,39 +4,31 @@ import {
   createStore,
 } from 'redux';
 
-export const deleteName = () => ({  
-  type: 'DELETE_NAME',
-  name: ''
-});
-
-// 引数nameをとり、{type: "ADD_NAME", name: name}を返すjsの関数。
-export const setName = name => ({  
-  type: 'ADD_NAME',
-  name: name,
-});
-
+// カードの合計を増やす
 export const upCount = sum => ({
   type: "UP_COUNT",
   sum,
 });
 
+// カードの合計を減らす
 export const downCount = sum => ({
   type: "DOWN_COUNT",
   sum,
 });
 
+// カード自体を増やす
+export const addCard = title => ({
+  type: "DOWN_COUNT",
+  title,
+});
+
 
 INITIAL_STATE = {
-  name: 'Nanasi',
   sum: 10,
 }
 
 const reducer = (state = INITIAL_STATE, action) => {  
   switch (action.type) {
-    case 'ADD_NAME':
-      return {...state, name: action.name}
-    case 'DELETE_NAME':
-      return {...state, name: ''}
     case "UP_COUNT":
       return {...state, sum: action.sum + 1}
     case "DOWN_COUNT":
